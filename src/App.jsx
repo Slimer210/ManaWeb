b2e452439e6bc9c31b7003dfb748219e9aece37a
Slimer210 <howweishan0210@outlook.com>
2022-06-07 17:52:19 +0800

    7/6/2022 MANAWEB UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
 import React from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
+import manaLogo from './assets/manalogo.png'
 
 const navigation = [
-  { name: '下載', href: '#', current: true },
+  { name: '下載', href: '#', current: false },
   { name: '手冊', href: '#', current: false },
   { name: '公告', href: '#', current: false },
   { name: '關於', href: '#', current: false },
   { name: '支持我們', href: '#', current: false },
 ]
 
-function navigationBar() {
-
-}
-
-function MyComponent() {
+function NavigationBar() {
   return (
-    <div>Fcuk my liv</div>
+    <div>
+      <div className="flex justify-between item-center px-5 py-5 drop-shadow-md bg-white">
+        <div className="flex-shrink-0 flex items-center px-3 py-3 active:text-mana-dark-purple hover:text-mana-dark-purple duration-300 hover:scale-110" href="#">
+          <img
+            className="block w-12 h-12 drop-shadow-none"
+            src={manaLogo}
+            alt="ManaMetal LOGO" />
+          <h1 className="px-3">魔法金屬官方網站</h1></div>
+        <div className=" flex flex-nowrap items-center justify-center gap-x-5">
+          {navigation.map((item, index) => (
+            <a className={`px-6 py-4 content-center whitespace-nowrap rounded-full active:text-mana-dark-purple hover:text-mana-dark-purple hover:scale-110 duration-300  ${index === 4 ? "bg-mana-purple text-white hover:text-white hover:bg-mana-dark-purple" : " "}`} key={item.name} href={item.href}>{item.name}</a>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
 function App() {
   const myVariable = "my"
   return (
-  <Disclosure as="nav">
-    <div className="flex justify-between px-5 py-5 drop-shadow-xl">
-      <h1>MANAWeb</h1>
-    <div className="items-right">
-    {navigation.map((item) => (
-    <a className="px-8 py-5 "key={item.name} href={item.href}>{item.name}</a>
-  ))}
-</div></div>
-</Disclosure>
+    <NavigationBar/>
   )
-  
 }
 
 export default App
