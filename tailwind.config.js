0c9eb3954741a284962054d0836130b7cf850e83
Slimer210 <howweishan0210@outlook.com>
2022-06-08 08:41:55 +0800

    8/6//2022 NavBar Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,23 +5,24 @@ module.exports = {
   ],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      mobile: '480px',
+      tablet: '768px',
+      laptop: '960px',
+      desktop: '1440px',
     },
-    colors: {
-      'mana-black': '#454545',
-      'mana-purple': '#8c82ff',
-      'patreon-orange': '#ff6855',
-      'paypal-blue': '#003087',
 
-    },
     fontFamily: {
       sans: ['Noto Sans TC', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
     },
     extend: {
+      colors: {
+        'mana-black': '#454545',
+        'mana-dark-purple': "#4D488C",
+        'mana-purple': '#8c82ff',
+        'patreon-orange': '#ff6855',
+        'paypal-blue': '#003087',
+      },
       spacing: {
         '128': '32rem',
         '144': '36rem',
